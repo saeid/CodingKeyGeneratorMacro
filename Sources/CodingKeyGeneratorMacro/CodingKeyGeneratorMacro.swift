76f181fdd361825ef9cd02d0d2e52075304a58ef
Saeid <dev@basirnia.xyz>
2023-09-07 14:32:01 +0200

    initial commit

--- Sources/CodingKeyGeneratorMacro/CodingKeyGeneratorMacro.swift
+++ Sources/CodingKeyGeneratorMacro/CodingKeyGeneratorMacro.swift
@@ -1,11 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-/// A macro that produces both a value and a string containing the
-/// source code that generated the value. For example,
-///
-///     #stringify(x + y)
-///
-/// produces a tuple `(x + y, "x + y")`.
-@freestanding(expression)
-public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "CodingKeyGeneratorMacroMacros", type: "StringifyMacro")
+@attached(member, names: arbitrary)
+public macro CodingKeyGen<T>(
+    properties: [PartialKeyPath<T>: String] = [:]
+) = #externalMacro(
+    module: "CodingKeyGeneratorMacroMacros",
+    type: "CodingKeyGen"
+)
---
+++ Sources/CodingKeyGeneratorMacroMacros/CodingKeyGeneratorError.swift
@@ -0,0 +1,10 @@
+//
+//  CodingKeyGeneratorError.swift
+//  
+//
+//  Created by Saeid Basirnia on 2023-09-07.
+//
+
+enum CodingKeyGeneratorError: String, Error {
+    case requiresStruct = "Macro needs a Struct"
+}
